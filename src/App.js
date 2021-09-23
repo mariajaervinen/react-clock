3d2702bb66c98ba2583a7bf4a23f16b372699495
Maria Järvinen <n9jama00@students.oamk.fi>
2021-09-23 15:47:30 +0300

    clock harjoitus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() =>{
+    console.log('componentdidmount');
+    const id = setInterval(()=>{
+      setTime(new Date());
+    },1000);
+    return() =>{
+      console.log('componentwillunmount');
+      clearInterval(id);
+    };
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style= {{margin:'30px'}}>
+      {time.toLocaleTimeString()}
     </div>
   );
 }
